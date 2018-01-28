d6cf81c5aaab87a5cde12eef9bfc984bc75297f2
mumby0168 <billy.mumby@outlook.com>
2018-01-28 15:53:47 +0000

    Scoresheet and program commented and removed the unused highscore class

--- ConsoleCopterGameFinal/Classes/ScoreSheet.cs
+++ ConsoleCopterGameFinal/Classes/ScoreSheet.cs
@@ -7,54 +7,62 @@ using System.Threading.Tasks;
 
 namespace ConsoleCopterGameFinal.Classes
 {
+    //this is the scoresheet class which inherits from the globals class
     class ScoreSheet : Globals
     {
-        static string _fileLoc = AppDomain.CurrentDomain.BaseDirectory + "Scoresheet.txt";
-        public List<int> _scores = new List<int>();
-        public List<string> _names = new List<string>();
-        private static List<string> _namesandScores = new List<string>();
+        //static declarations
+        static string _fileLoc = AppDomain.CurrentDomain.BaseDirectory + "Scoresheet.txt"; // this declares a static string which will store the location of the file
+        static List<string> _namesandScores = new List<string>(); // this declares a list of strings which will be used to store both the name and score of the player
 
-
+        //public delcarations
+        public List<int> _scores = new List<int>(); //this is a list of integres
+        public List<string> _names = new List<string>(); // this is a list of strings
+        
+        //this is the constructor for the scoresheet class
         public ScoreSheet()
         {
-            Read();
+            Read(); // this will call the public read method and will be called when a instance of the object has been created
         }
+
+        //this is a public method that will take two arguments name and score
         public void Write(string name, int score)
         {
-            _names.Add(name);
-            _scores.Add(score);
-            StreamWriter writer = new StreamWriter(_fileLoc);
-            for (int i = 0; i < _names.Count; i++)
+            _names.Add(name);//this will add the name paramater that will be passed to the names list
+            _scores.Add(score);//this will add the score paramater that will be passed to the scores list
+            StreamWriter writer = new StreamWriter(_fileLoc); // this opens the streamreader at the passed location
+            for (int i = 0; i < _names.Count; i++) // this will loop over until it meets the count of the names list
             {
-                writer.WriteLine(_names[i]);
-                writer.WriteLine(_scores[i].ToString());
+                writer.WriteLine(_names[i]); // this will add a name to the list
+                writer.WriteLine(_scores[i].ToString());// this will add a score to the list and convert it to a string
             }
 
-            writer.Close();
+            writer.Close(); // this will close the writer
         }
 
+        //this will read the exising names and scores in the text file
         public void Read()
         {
-            StreamReader reader = new StreamReader(_fileLoc);
-            int lines = File.ReadLines(_fileLoc).Count();
-            for (int i = 0; i < lines * 0.5; i++)
+            StreamReader reader = new StreamReader(_fileLoc); // this open a new reader using the file that is passed
+            int lines = File.ReadLines(_fileLoc).Count(); // this declares a variable which will get how many lines there are in the file that is passed
+            for (int i = 0; i < lines * 0.5; i++) // this will loop until it has met half the value of the lines int
             {
-                _names.Add(reader.ReadLine());
-                _scores.Add(int.Parse(reader.ReadLine()));
+                _names.Add(reader.ReadLine()); // this will add a name to the score reading from the file
+                _scores.Add(int.Parse(reader.ReadLine())); // this will add a score and parse it which is read from the file
             }
 
-            reader.Close();
+            reader.Close(); // this will close the reader
         }
 
+        //this will get the list that can be displayed in the highscore screen it will return a list
         public List<string> GetList()
         {
 
-            for (int i = 0; i < _names.Count; i++)
+            for (int i = 0; i < _names.Count; i++) // this will loop until it has met the count of the names list
             {
-                _namesandScores.Add("    "+_names[i] + " " + _scores[i]);
+                _namesandScores.Add("    "+_names[i] + " " + _scores[i]); // this will add the names and scores to the list with spaces
             }
 
-            return _namesandScores;
+            return _namesandScores; // this will return the names and scores list
         }
     }
 }
--- ConsoleCopterGameFinal/Program.cs
+++ ConsoleCopterGameFinal/Program.cs
@@ -17,40 +17,38 @@ namespace ConsoleCopterGameFinal
         private static Random
             Rand = new Random(DateTime.Today.Millisecond); // delcares a random object with a seed of todays millsecond
 
-        private static ScoreSheet scoreTable = new ScoreSheet();
+        private static ScoreSheet scoreTable = new ScoreSheet(); // this creates a static instance of the scoresheet class
         private static Copter copter = new Copter(); // creates a static instance of the copter class
         private static List<Obstacles> _obstacles = new List<Obstacles>(); // creates a private static 
-        private static MenuSystem Menu = new MenuSystem();
-        private static ScoreSheet FileLog = new ScoreSheet();
+        private static MenuSystem Menu = new MenuSystem(); // this creats a static instance of the menuSystem class
+        private static ScoreSheet FileLog = new ScoreSheet(); // this creates a static instance of the scoresheet class
 
         //thread declarations
         static Thread _updateCopter; // declaring a variable for the update copter loop
-        static string name = "";
-
 
         static void Main(string[] args)
         {
-            Menu.Show(FileLog.GetList());
+            Menu.Show(FileLog.GetList()); // this will show the menu and get the list from the file log object and pass it the list is retreives
             DrawObstacles(15); // this calls the draw obstacles method
-            GameRunning = true; // stes the game running boolean to true
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Green;
+            GameRunning = true; // sets the game running boolean to true
             _updateCopter = new Thread(copter.Update); //instanciates and calls update method on the copter
             _updateCopter.Start(); // starts the copter update loop
 
-            if (copter.CheckCollison(_obstacles, copter)
-            ) //if the copter check collision returns true then do in the block
+            if (copter.CheckCollison(_obstacles, copter)) //if the copter check collision returns true then do in the block
             {
-                GameRunning = false;
-                Console.Beep(1000, 1000);
-                FileLog.Write(Name,GameScore);
-                DialogResult result =  MessageBox.Show("Play Again?","GAME OVER", MessageBoxButtons.YesNo);
-
-                if (result == DialogResult.Yes)
+                GameRunning = false; //this will set the game loop to false
+                Console.Beep(1000, 1000);//this will make the console beep for 1 second
+                FileLog.Write(Name,GameScore);//this will tell the file log object to write the name and score to the file
+                DialogResult result =  MessageBox.Show("Play Again?","GAME OVER", MessageBoxButtons.YesNo); //this will show a message box with a yes and no button and record the result in the dialog result object
+                if (result == DialogResult.Yes) //this will check to see if they selected option is yes
                 {
-                    Application.Restart();
+                    Application.Restart();//this will restart the application
                 }
-                else if(result == DialogResult.No)
+                else if(result == DialogResult.No) //this will check to see if the option chose is no
                 {
-                    Environment.Exit(1);
+                    Environment.Exit(1); //this will close the application
                 }
             }
         }
